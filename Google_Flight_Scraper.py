3de1ca2af737f8bd13de1b3f71094e5e719a7430
jjc9991 <>
2022-06-11 20:21:12 +0000

    Created files for class definitions.

---
+++ Google_Flight_Scraper.py
@@ -0,0 +1,4 @@
+# This class was created to access and store the flight data from the Google Flights page.  This is the class that utilizes Selemium to gather the data based on the user inputs (data collected in userData class).
+
+class googleFS:
+  def __init__(self):
---
+++ userData.py
@@ -0,0 +1,5 @@
+class userData:
+  def __init__(self):
+    # Departure and arrival airports
+    # Departure and arrival dates
+    # Flight type (round trip vs. one-way)
