9f160e2b8fb84cad17773053405b4d1ea1ef904e
jjc9991 <>
2022-06-26 23:13:57 +0000

    Commented out call to gather user data (for now) and added lines to figure out how to use Selenium and open the browser in replit.

--- Run_Flight_Scraper.py
+++ Run_Flight_Scraper.py
@@ -1,4 +1,14 @@
 from userData import userData
+from Selenium_Flight_Scraper_Tool import seleniumFS
 
-user = userData()
+# -----------------
+# Gather User Data
+# -----------------
+# user = userData()
 # user.get_user_data() # print user data to confirm I got it correctly
+
+# -----------------------------
+# Use Selenium to open browser
+# -----------------------------
+SFS = seleniumFS()
+SFS.open_browser()
