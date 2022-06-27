baed783947633308e942303f119de06ed14fa4b9
jjc9991 <>
2022-06-27 00:50:16 +0000

    Added dictionary of xPaths and started working on writing the code to find the departure airport textbox on the website.  Next I need to confirm that I have the right xPath and actually input the user data onto the page using Keys.

--- Selenium_Flight_Scraper_Tool.py
+++ Selenium_Flight_Scraper_Tool.py
@@ -2,6 +2,7 @@
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
 
 class seleniumFS:
   def __init__(self):
@@ -11,5 +12,12 @@ class seleniumFS:
     self.browser = webdriver.Chrome('chromedriver', options=chrome_options) # This is the path when using replit
     self.google_flights_url = "https://www.google.com/travel/flights"
 
+    # Dictionary of xPaths for google flights website
+    self.xPaths = {'departure_airport': '//*[@id="ow44"]/div[1]/div/div/input'}
+
   def open_browser(self):
     self.browser.get(self.google_flights_url)
+
+  def input_user_data(self, user):
+    self.browser.find_element_by_xpath(self.xPaths['departure_airport'])
+    # TODO: input user data at this xPath (note that the xPath I am using now might not be correct) using Keys
