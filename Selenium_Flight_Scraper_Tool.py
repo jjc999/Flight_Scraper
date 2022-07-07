ca16dfff9d14d9b97ef3edf71d3e90dfa78dd227
jjc9991 <>
2022-07-07 00:10:41 +0000

    Updated some comments, tried to figure out how to access the date element on the page.

--- Selenium_Flight_Scraper_Tool.py
+++ Selenium_Flight_Scraper_Tool.py
@@ -15,7 +15,8 @@ class seleniumFS:
 
     # Dictionary of xPaths for google flights website
     self.xPaths = {'departure_airport': "//*[@id='i14']/div[6]/div[2]/div[2]/div[1]/div/input",
-                   'arrival_airport': "//*[@id='i14']/div[6]/div[2]/div[2]/div[1]/div/input"}
+                   'arrival_airport': "//*[@id='i14']/div[6]/div[2]/div[2]/div[1]/div/input", 
+                   'departure_date': "//*[@id='ow62']/div[1]/div/div/div[1]/div/div[1]/div/input"}
 
   def open_browser(self):
     self.browser.get(self.google_flights_url)
@@ -23,7 +24,11 @@ class seleniumFS:
   def input_user_data(self, user):
     # Recent Progress and thoughts
     # -----------------------------
-    # I figured out that the departure airport box is the default active element, so I can just click on that to be able to input text.  Once I click on it, I am able to get to the text element that I can actually use Selenium to interact with (what the xpath now references).  Once I get that, I can send the departure airport text to the box.  I then click ENTER to return to the search page and then hit TAB to move over to the arrival airport box.  My strategy now is to try finding the xpaths for each element following this approach and to google around and see if other people have done this and can just give me the xpaths.
+    # I figured out that the departure airport box is the default active element, so I can just click on that to be able to input text.
+    # Once I click on it, I am able to get to the text element that I can actually use Selenium to interact with (what the xpath now 
+    # references).  Once I get that, I can send the departure airport text to the box.  I then click ENTER to return to the search 
+    # page and then hit TAB to move over to the arrival airport box.  My strategy now is to try finding the xpaths for each element 
+    # following this approach and to google around and see if other people have done this and can just give me the xpaths.
     # -----------------------------
     # Enter depature airport
     departure_airport_box = self.browser.switch_to.active_element  # Departure Airport box
@@ -39,7 +44,12 @@ class seleniumFS:
     arrival_airport_text = self.browser.find_element("xpath", self.xPaths['arrival_airport'])
     arrival_airport_text.send_keys(user.arrival_airport)
     arrival_airport_text.send_keys(Keys.ENTER)
-    arrival_airport_box.send_keys(Keys.TAB)  # Move over to date box
+
+    # Enter departure date
+    arrival_airport_box.send_keys(Keys.TAB)
+    # departure_date_box = self.browser.find_element("xpath", self.xPaths['departure_date'])
+    departure_date_box = self.browser.switch_to.active_element
+    departure_date_box.click()
 
     # Enter dates
 
